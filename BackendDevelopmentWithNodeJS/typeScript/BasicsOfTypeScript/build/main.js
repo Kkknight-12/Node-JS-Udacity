53e01120c7582b58adeffbdeee732f3618ad8a97
Knight <mayanks365@gmail.com>
2021-07-16 11:50:50 +0530

    commiting basic and ExerciseProject files

---
+++ BackendDevelopmentWithNodeJS/typeScript/BasicsOfTypeScript/build/main.js
@@ -0,0 +1,17 @@
+"use strict";
+var a = 4;
+var b = 6;
+var c = "5";
+var d = "cat";
+var multiply = function (num1, num2) {
+    console.log(num1 + " * " + num2 + " =", num1 * num2);
+};
+multiply(a, b);
+multiply(a, c);
+multiply(a, d);
+var add = function (num1, num2) {
+    console.log(num1 + " + " + num2 + " =", num1 + num2);
+};
+add(a, b);
+add(a, c);
+add(a, d);
